69a969a02a5d70e50dc04f6b68db82621e46c906
Prashant Negi <prashantnegia@gmail.com>
2024-01-13 15:38:56 +0530

    Solved leetcode929

---
+++ src/array/and/hashing/LeetCode929.java
@@ -0,0 +1,28 @@
+package array.and.hashing;
+import java.util.*;
+// Unique Email Addresses https://leetcode.com/problems/unique-email-addresses/description/
+public class LeetCode929 {
+    public static void main(String[] args) {
+        String[] emails = {"test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"};
+        Set<String> uniqueEmails = new HashSet<>();
+        for(String email : emails) {
+            String formattedEmail = getFormattedEmail(email);
+            uniqueEmails.add(formattedEmail);
+        }
+
+        System.out.println(uniqueEmails.size());
+    }
+
+    static String getFormattedEmail(String email) {
+        String[] arr = email.split("@");
+        String localName = arr[0];
+        String domainName = arr[1];
+
+        String[] arrLocalWithPlus = localName.split("\\+");
+        localName = arrLocalWithPlus[0];
+
+        localName = localName.replace(".", "");
+
+        return localName + "@" + domainName;
+    }
+}
