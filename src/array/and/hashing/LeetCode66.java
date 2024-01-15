d02dd3a6379d8dcc4dcf4d79536db9e77f89223b
Singh Negi <prashant.singh.negi@accenture.com>
2024-01-15 18:18:00 +0530

    solved leetcode 66

---
+++ src/array/and/hashing/LeetCode66.java
@@ -0,0 +1,36 @@
+package array.and.hashing;
+import java.util.*;
+public class LeetCode66 {
+    public static void main(String[] args) {
+        int[] digits = {4, 1, 9};
+
+        System.out.println(Arrays.toString(Method(digits)));
+    }
+
+    static int[] Method(int[] digits) {
+        int len = digits.length - 1;
+        int[] result = new int[digits.length + 1];
+        boolean carry = true;
+
+        for (int i = len; i >= 0; i--) {
+            if (carry) {
+                if ((digits[i] + 1) > 9) {
+                    digits[i] = (digits[i] + 1) % 10;
+                } else {
+                    digits[i] = digits[i] + 1;
+                    carry = false;
+                }
+            }
+        }
+
+        if (carry) {
+            result[0] = 1;
+            for (int i = 0; i < len + 1; i++) {
+                result[i + 1] = digits[i];
+            }
+            return result;
+        }
+
+        return digits;
+    }
+}
