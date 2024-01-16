a7019221472fb417871a33376550a4bf5659387b
Prashant Negi <prashantnegia@gmail.com>
2024-01-16 17:35:13 +0530

    solved leetcode 14

--- src/array/and/hashing/LeetCode14.java
+++ src/array/and/hashing/LeetCode14.java
@@ -1,2 +1,23 @@
-package array.and.hashing;public class LeetCode14 {
+package array.and.hashing;
+
+// Longest Common Prefix https://leetcode.com/problems/longest-common-prefix/description/
+public class LeetCode14 {
+    public static void main(String[] args) {
+        String[] strs = {"flower", "flow", "flight"};
+        System.out.println(Method(strs));
+    }
+
+    static String Method(String[] strs) {
+        if(strs.length == 0)
+            return "";
+
+        String prefix = strs[0];
+        for(int i = 1; i < strs.length; i++) {
+            while(strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+            }
+        }
+
+        return prefix;
+    }
 }
