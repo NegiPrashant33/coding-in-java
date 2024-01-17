b8dd2a0d972d57f8cacf0ad799d96291de072171
Prashant Negi <prashantnegia@gmail.com>
2024-01-17 18:39:09 +0530

    solved leetcode 49

--- src/array/and/hashing/LeetCode49.java
+++ src/array/and/hashing/LeetCode49.java
@@ -1,2 +1,31 @@
-package array.and.hashing;public class LeetCode49 {
+package array.and.hashing;
+import java.util.*;
+
+// Group anagrams https://leetcode.com/problems/group-anagrams/description/
+
+public class LeetCode49 {
+    public static void main(String[] args) {
+        String[] strs = {"eat", "tea", "tan", "ate", "nat"," bat"};
+
+        List<List<String>> result = Method(strs);
+        System.out.println(result);
+    }
+
+    static List<List<String>> Method(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for(String word : strs) {
+            char[] chars =  word.toCharArray();
+            Arrays.sort(chars);
+            String sortedWord = new String(chars);
+
+            if(!map.containsKey(sortedWord))
+                map.put(sortedWord, new ArrayList<>());
+
+            map.get(sortedWord).add(word);
+        }
+
+
+        return new ArrayList<>(map.values());
+    }
 }
