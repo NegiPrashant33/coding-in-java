234a9282767bc4225064faead3e8ef7118697d26
Prashant Negi <prashantnegia@gmail.com>
2024-01-21 12:29:37 +0530

    Solved leetcode 58

--- src/array/and/hashing/LeetCode58.java
+++ src/array/and/hashing/LeetCode58.java
@@ -1,2 +1,37 @@
-package array.and.hashing;public class LeetCode58 {
+package array.and.hashing;
+
+// Length of Last Word https://leetcode.com/problems/length-of-last-word/description/
+public class LeetCode58 {
+    public static void main(String[] args) {
+        String s = "  fly me  to the   moon  ";
+        System.out.println(Method1(s));
+    }
+
+    static int Method1(String s) {
+        int track = 0;
+        for(int i = s.length() - 1; i >= 0; i--) {
+            if(s.charAt(i) != ' ' && s.charAt(i - 1) == ' ') {
+                track = i;
+                break;
+            }
+        }
+
+        String result = s.substring(track).trim();
+        return result.length();
+    }
+
+    static int Method2(String s) {
+        int count = 0;
+        for(int i = s.length() - 1; i >= 0; i--) {
+            if(s.charAt(i) != ' ') {
+                count++;
+            }
+            else {
+                if(count > 0)
+                    return count;
+            }
+        }
+
+        return count;
+    }
 }
