8d34748d09edfcc1f41427dfeb270d3fde257b33
Prashant Negi <prashantnegia@gmail.com>
2024-02-02 23:18:20 +0530

    Solved Leetcode 169

---
+++ src/array/and/hashing/LeetCode169.java
@@ -0,0 +1,23 @@
+package array.and.hashing;
+import java.util.*;
+public class LeetCode169 {
+    public static void main(String[] args) {
+        int[] nums = {2, 2, 1, 1, 1, 2, 2};
+        System.out.println(Method(nums));
+    }
+
+    static int Method(int[] nums) {
+        Map<Integer, Integer> count = new HashMap<>();
+        for(int num : nums) {
+            count.put(num, count.getOrDefault(num, 0) + 1);
+        }
+
+        for(int key : count.keySet()) {
+            if(count.get(key) > (nums.length / 2)) {
+                return key;
+            }
+        }
+
+        return 0;
+    }
+}
