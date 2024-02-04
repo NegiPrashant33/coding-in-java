9e171bd8243ee0410e6ba80ec2145c07687b3b7d
Prashant Negi <prashantnegia@gmail.com>
2024-02-04 13:14:29 +0530

    Solved Leetcode 496

---
+++ src/array_and_hashing/LeetCode496.java
@@ -0,0 +1,33 @@
+package array_and_hashing;
+import java.util.*;
+// Next Greater Element     https://leetcode.com/problems/next-greater-element-i/description/
+public class LeetCode496 {
+    public static void main(String[] args) {
+        int[] nums1 = {4, 1, 2};
+        int[] nums2 = {1, 3, 4, 2};
+
+        System.out.println(Arrays.toString(Method(nums1, nums2)));
+    }
+
+    private static int[] Method(int[] nums1, int[] nums2) {
+        int[] ans = new int[nums1.length];
+
+        for(int i = 0; i < nums1.length; i++) {
+            int target = nums1[i];
+            int nextGreater = 0;
+            boolean check = false;
+            for(int j = 0; j < nums2.length; j++) {
+                if(nums2[j] == target) {
+                    check = true;
+                }
+                if(check && target < nums2[j]) {
+                    nextGreater = nums2[j];
+                    break;
+                }
+            }
+            ans[i] = (nextGreater != 0) ? nextGreater : -1;
+        }
+
+        return ans;
+    }
+}
