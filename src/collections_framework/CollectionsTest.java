8eafa52607fab54039aa0b96dfc33aaf52b744ff
Prashant Negi <prashantnegia@gmail.com>
2024-02-07 08:28:20 +0530

    Iterable and Iterator implementation

--- src/collections_framework/CollectionsTest.java
+++ src/collections_framework/CollectionsTest.java
@@ -1,2 +1,33 @@
-package collections_framework;public class CollectionsTest {
+package collections_framework;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/*
+
+    Testing Implementation of Iterable and Interator interface
+
+*/
+public class CollectionsTest {
+    public static void main(String[] args) {
+        OurGenericList<Integer> list = new OurGenericList<>();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        /*
+            Iterator<Integer> iterator = list.iterator();
+            while(iterator.hasNext())
+                System.out.println(iterator.next());
+
+         */
+
+        for(int x : list)
+            System.out.println(x);
+
+
+        List<Integer> lst = new ArrayList<>();
+    }
 }
--- src/collections_framework/OurGenericList.java
+++ src/collections_framework/OurGenericList.java
@@ -1,2 +1,47 @@
-package collections_framework;public class OurGenericList {
+package collections_framework;
+
+import java.util.Iterator;
+
+/*
+    Demo Implementation of Iterable and Interator interface
+*/
+public class OurGenericList <T> implements Iterable<T> {
+    private T[] items;
+    private int size;
+
+    public OurGenericList() {
+        size = 0;
+        items = (T[])new Object[100];
+    }
+
+    public void add(T item) {
+        items[size++] = item;
+    }
+
+    public T getItemAtIndex(int index) {
+        return items[index];
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new OurGenericListIterator(this);
+    }
+
+    private class OurGenericListIterator implements Iterator<T> {
+
+        private OurGenericList<T> list;
+        private int index = 0;
+        public OurGenericListIterator(OurGenericList<T> list) {
+            this.list = list;
+        }
+        @Override
+        public boolean hasNext() {
+            return index < list.size;
+        }
+
+        @Override
+        public T next() {
+            return list.items[index++];
+        }
+    }
 }
